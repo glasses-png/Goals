1e5b66db2c403b0f9f9dc974bc5dddc6ffb3f20d
glasses-png <cchelangat16@gmail.com>
2020-04-10 10:17:14 +0300

    changes

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+import { AlertService } from '../alert-service/alert.service';
+import { HttpClient } from '@angular/common/http';
+import { Quote } from '../quote-class/quote';
+import { QuoteRequestService } from '../quote-http/quote-request.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-goal',
@@ -7,6 +13,11 @@ import { Goal } from '../goal';
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
+  goToUrl(id){
+    this.router.navigate(['/goals',id])
+  }
+  
+  
   goals: Goal[] = [
       new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
       new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
@@ -15,32 +26,53 @@ export class GoalComponent implements OnInit {
       new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
       new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
   ];
+  goal: typeof Goal;
+  alertService: AlertService;
+  quote:Quote;
+
+
   addNewGoal(goal){
     let goalLength = this.goals.length;
     goal.id = goalLength+1;
     goal.completeDate = new Date(goal.completeDate)
     this.goals.push(goal)
   }
-  toggleDetails(index){
-    this.goals[index].showDescription = !this.goals[index].showDescription;
-  }
-  completeGoal(isComplete, index){
-    if (isComplete) {
-      this.goals.splice(index,1);
-    }
-  }
-  deleteGoal(isComplete, index){
-    if (isComplete) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+  // toggleDetails(index){
+  //   this.goals[index].showDescription = !this.goals[index].showDescription;
+  // }
+  // completeGoal(isComplete, index){
+  //   if (isComplete) {
+  //     this.goals.splice(index,1);
+  //   }
+  // }
+  deleteGoal(index){
+    let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}`)
 
-      if (toDelete){
-        this.goals.splice(index,1)
-      }
+    if (toDelete){
+      this.goals.splice(index,1)
+      this.alertService.alertMe("Goal has been deleted")
     }
   }
-    constructor() { }
- 
+  
+  constructor(goalService:GoalService, alertService:AlertService, private http:HttpClient, private quoteService:QuoteRequestService, private router:Router) {
+    this.goals = goalService.getGoals()
+    this.alertService = alertService;
+  }
     ngOnInit() {
+      // interface ApiResponse{
+      //   author:string;
+      //   quote:string; 
+        this.quoteService.quoteRequest()
+        this.quote=this.quoteService.quote
     }
+    // this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
+      // Succesful API request
+  //     this.quote = new Quote(data.author, data.quote)
+  // }, 
+  // err=>{
+  //   this.quote=new Quote("Wiston Churchill", "Never never give up!")
+  //   console.log("An error occurred")
+  //   })
+  // }
   
   }
